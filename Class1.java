12e12f93d64074177969a1556920eb4ee4a1498c
William Penwell <wbpenwell@live.com>
2019-01-14 13:25:28 -0600

    CS310

--- Class1.java
+++ Class1.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author William Penwell
  */
 
 public class Class1 {
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author William Penwell
  */
 
 public class HelloWorld {
